8b7d84086463a967d984f7e7fdab77f1ff0f8b29
nasimbayati <nasim.bayati1993@gmail.com>
2026-04-26 15:28:05 -0500

    Modified the CLI Menu to match the instructions

--- cli/src/main/java/edu/acc/neonark/cli/NeonArkCliApplication.java
+++ cli/src/main/java/edu/acc/neonark/cli/NeonArkCliApplication.java
@@ -63,8 +63,9 @@ public class NeonArkCliApplication {
 
     private void printMenu() {
         System.out.println("=====================================");
-        System.out.println("NEON ARK CLI SYSTEM");
+        System.out.println("     NEON ARK CLI SYSTEM");
         System.out.println("=====================================");
+        System.out.println();
         System.out.println("1. List all creatures");
         System.out.println("2. View creature by ID");
         System.out.println("3. Register new creature");
@@ -72,8 +73,10 @@ public class NeonArkCliApplication {
         System.out.println("5. Remove creature");
         System.out.println("6. View creature observations/notes");
         System.out.println("7. Find creatures by feeding time");
+        System.out.println();
         System.out.println("--- Admin Only ---");
         System.out.println("8. View all system users");
+        System.out.println();
         System.out.println("0. Exit");
         System.out.println("-------------------------------------");
         System.out.print("Select an option: ");
